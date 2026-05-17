9bd11bac588e551762488d873f8c0ff0220c144c
Kirill Katz <xnlyre@protonmail.com>
2026-05-17 17:06:36 +0300

    Solve

--- lua/config/snippets/cpp.lua
+++ lua/config/snippets/cpp.lua
@@ -7,7 +7,13 @@ return {
     s("cf", {
         t({
             "#include <bits/stdc++.h>",
+            "#include <ext/pb_ds/assoc_container.hpp>",
+            "#include <ext/pb_ds/tree_policy.hpp>",
             "using namespace std;",
+            "using namespace __gnu_pbds;",
+            "",
+            "template <class T>",
+            "using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;",
             "",
             "void solve();",
             "",
